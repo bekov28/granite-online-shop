111ab83be91d5f0f05e11910e925ffb5a66baab2
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-08 21:45:28 +0900

    productService

--- src/utility/firebaseConfig.js
+++ src/utility/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import { getFirestore, gitFireStore } from 'firebase/fireStore'
+import { getFirestore} from 'firebase/fireStore'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
