5b5441d4f58867e148249e50fe261b204006177b
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-12 18:40:50 +0900

    delete product

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,12 +5,33 @@ const productCollection = collection(db, 'products')
 
 export default {
   async createProduct(productData) {
+    //post method
     const docRef = await addDoc(productCollection, productData)
     return { id: docRef.id, ...productData }
   },
 
   async getProducts() {
+    //getAll method
     const snaphost = await getDocs(productCollection)
-    return snaphost.docs.map((doc) => ({id: doc.id, ...doc.data()}))
-  }
+    return snaphost.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  },
+
+  async getProduct(id) {
+    //get method
+    const docRef = doc(db, 'products', id)
+    const snaphost = await getDoc(docRef)
+    return { id: snaphost.id, ...snaphost.data() }
+  },
+
+  async updateProduct(id, productData) {
+    //get method
+    const docRef = doc(db, 'products', id)
+    await updateDoc(docRef, productData)
+    return { id, ...productData }
+  },
+
+  async deleteDoc(id) {
+    const docRef = doc(db, 'products', id)
+    await deleteDoc(docRef)
+  },
 }
