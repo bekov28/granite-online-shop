e1283efa01149b9cc69a31c2c21a501abe6a3a41
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-20 08:21:29 +0900

    sign out done

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -9,6 +9,7 @@ import {
   onAuthStateChanged,
 } from 'firebase/auth'
 import { ROLE_ADMIN, ROLE_USER } from '@/constants/appConstants'
+import { useSwal } from '@/utility/useSwal'
 
 export const useAuthStore = defineStore('authStore', () => {
   const user = ref(null)
@@ -16,6 +17,7 @@ export const useAuthStore = defineStore('authStore', () => {
   const isLoading = ref(false)
   const role = ref(null)
   const initialized = ref(false) //if authorization has been initialized
+  const { showSuccess } = useSwal()
 
   const isAuthenticated = computed(() => user.value !== null)
   const isAdmin = computed(() => role.value === ROLE_ADMIN)
@@ -63,9 +65,25 @@ export const useAuthStore = defineStore('authStore', () => {
     isLoading.value = true
     try {
       const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+      showSuccess('Successfully Signed In')
       user.value = userCredentials.user //saving user object to user.value
       user.role = ROLE_USER
       user.value = userCredentials.user
+      error.value = null //if everything is valid
+    } catch (err) {
+      error.value = err.message
+      throw err
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const signOutUser = async () => {
+    isLoading.value = true
+    try {
+      await signOut(auth)
+      showSuccess('Successfully Signed Out')
+      clearUser()
       error.value = null //if everything is valid
     } catch (err) {
       error.value = err.message
@@ -90,5 +108,6 @@ export const useAuthStore = defineStore('authStore', () => {
     signUpUser,
     signInUser,
     initializeAuth,
+    signOutUser,
   }
 })
